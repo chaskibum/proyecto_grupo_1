4beb51a7903e97fac264f972f2f53a66149fbd5e
Bruno Giacometti <152458112+chaskibum@users.noreply.github.com>
2025-10-10 08:22:28 -0300

    commit

--- js/theme.js
+++ js/theme.js
@@ -3,12 +3,14 @@ const body = document.body;
 const icon = button.querySelector("i"); // <i class="bi bi-moon-fill"></i>
 
 // Aplicar tema guardado al cargar la página
-if (localStorage.getItem("theme") === "dark") {
-    body.classList.add("dark-mode");
-    icon.className = "bi bi-sun-fill"; // mostrar sol
-} else {
-    icon.className = "bi bi-moon-fill"; // mostrar luna
-}
+addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("theme") === "dark") {
+        body.classList.add("dark-mode");
+        icon.className = "bi bi-sun-fill"; // mostrar sol
+    } else {
+        icon.className = "bi bi-moon-fill"; // mostrar luna
+    }
+});
 
 // Cambiar tema al hacer click
 button.addEventListener("click", () => {
@@ -34,5 +36,3 @@ button.addEventListener("click", () => {
        // localStorage.setItem("theme", "light");
    // }
 //});
-
-    
