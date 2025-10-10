b8f88ccdf91fca4c359b6142d0e4093a37d09052
Bruno Giacometti <152458112+chaskibum@users.noreply.github.com>
2025-10-10 10:36:10 -0300

    perfil

--- js/my-profile.js
+++ js/my-profile.js
@@ -35,4 +35,37 @@ button.addEventListener("click", () => {
    // }
 //});
 
+document.addEventListener("DOMContentLoaded", () => {
+    const fotoPerfil = document.getElementById("fotoPerfil");
+    const inputFoto = document.getElementById("inputFoto");
+    const agregar = document.getElementById("agregarFoto");
+
+
+    const fotoGuardada = localStorage.getItem("fotoPerfil");
+    if (fotoGuardada) {
+        fotoPerfil.className = "";
+        fotoPerfil.innerHTML = `<img src="${fotoGuardada}" alt="Foto de perfil" style="width:100px; height:100px; border-radius:50%;">`;
+    }
+
+    fotoPerfil.addEventListener("click", () => inputFoto.click());
+    agregar.addEventListener("click", () => inputFoto.click());
+    
+    inputFoto.addEventListener("change", () => {
+        const archivo = inputFoto.files[0];
+        if (archivo) {
+            const lector = new FileReader();
+            lector.onload = function(e) {
+                const imagenBase64 = e.target.result;
+                fotoPerfil.className = "";
+                fotoPerfil.innerHTML = `<img src="${imagenBase64}" alt="Foto de perfil" style="width:100px; height:100px; border-radius:50%;">`;
+                localStorage.setItem("fotoPerfil", imagenBase64);
+            };
+            lector.readAsDataURL(archivo);
+
+        }
+    });
+});
+
+
+
     
