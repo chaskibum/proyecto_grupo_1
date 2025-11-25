2b990ca44d178e5ee808522379e40f77bcd53171
santiagopena171 <santijoepena@gmail.com>
2025-11-25 20:45:17 -0300

    arreglo

--- js/init.js
+++ js/init.js
@@ -1,3 +1,16 @@
+const CATEGORIES_URL = "http://localhost:3000/emercado-api/cats/cat.json";
+const PUBLISH_PRODUCT_URL = "http://localhost:3000/emercado-api/sell/publish.json";
+const PRODUCTS_URL = "http://localhost:3000/emercado-api/cats_products/";
+const PRODUCT_INFO_URL = "http://localhost:3000/emercado-api/products/";
+const PRODUCT_INFO_COMMENTS_URL = "http://localhost:3000/emercado-api/products_comments/";
+const CART_INFO_URL = "http://localhost:3000/emercado-api/user_cart/";
+const CART_BUY_URL = "http://localhost:3000/emercado-api/cart/buy.json";
+const EXT_TYPE = ".json";
+
+let showSpinner = function () {
+  document.getElementById("spinner-wrapper").style.display = "block";
+}
+
 let hideSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "none";
 }
